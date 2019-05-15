74ae87e755f00e5e25203cfacd8f61b576befafd
Zoe Ames <zoeames@gmail.com>
2019-05-14 20:51:49 -0500

    pins print

--- src/javascripts/components/pins/pins.js
+++ src/javascripts/components/pins/pins.js
@@ -1,9 +1,18 @@
 import pinsData from '../../helpers/data/pinsData';
+import util from '../../helpers/util';
+
+const writePins = (pins) => {
+  let domString = '';
+  pins.forEach((pin) => {
+    domString += `<img src="${pin.imageUrl}" alt="pin image">`;
+  });
+  util.printToDom('pins-on-board', domString);
+};
 
 const initPins = (boardId) => {
   pinsData.loadPinsForBoard(boardId)
     .then((pins) => {
-      console.error('all pins', pins);
+      writePins(pins);
     })
     .catch(err => console.error(err));
 };
